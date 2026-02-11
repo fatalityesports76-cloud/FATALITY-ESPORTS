5793c84e4fcf0c3e378d852053b553a59fc14cc8
Fatality E-sports <fatalityesports76@gmail.com>
2026-02-11 14:45:49 -0300

    chore(auto): sync 2026-02-11 14:45:49

--- scripts/install-windows-autostart.ps1
+++ scripts/install-windows-autostart.ps1
@@ -11,15 +11,11 @@ $quotedScript = '"' + $scriptPath + '"'
 $taskCommand = "powershell.exe -NoProfile -ExecutionPolicy Bypass -File $quotedScript"
 
 # Recria para garantir parametros corretos.
-& schtasks /Delete /TN $taskName /F *> $null
+$deleteCmd = "schtasks /Delete /TN ""$taskName"" /F >nul 2>&1"
+cmd.exe /c $deleteCmd | Out-Null
 
-$createOutput = & schtasks `
-  /Create `
-  /F `
-  /TN $taskName `
-  /SC ONLOGON `
-  /RL LIMITED `
-  /TR $taskCommand 2>&1
+$createCmd = "schtasks /Create /F /TN ""$taskName"" /SC ONLOGON /RL LIMITED /TR ""$taskCommand"""
+$createOutput = cmd.exe /c $createCmd 2>&1
 
 if ($LASTEXITCODE -ne 0) {
   throw "Falha ao criar tarefa agendada: $createOutput"
